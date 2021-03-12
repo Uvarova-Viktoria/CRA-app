a5001ca4a8ef0724b983da437febd8609905b87b
Uvarova-Viktoria <59088574+Uvarova-Viktoria@users.noreply.github.com>
2021-03-12 13:24:32 +0300

    feat: add movieCredits element

---
+++ src/components/personalPage/movieCredits.js
@@ -0,0 +1,65 @@
+import React, { useEffect, useState } from "react";
+import FilmsSliderCard from "../filmSliderCard";
+import { css } from '@emotion/css';
+//const FEATURE_API =`https://api.themoviedb.org/3/person/${person_id}/movie_credits?api_key=c35b372cfa1b3f13b4f773b276d1de6e&language=ru`;
+
+
+/*
+const MovieCredits =({person_id}) =>(
+  
+  <div>
+      credits
+      {person_id}
+  </div>
+);
+export default  MovieCredits;*/
+
+export default function MovieCredits({person_id}){
+  const FEATURE_API =`https://api.themoviedb.org/3/person/${person_id}/movie_credits?api_key=c35b372cfa1b3f13b4f773b276d1de6e&language=ru`;
+  const [movie, setMovie] = useState([]);
+  useEffect(() => {
+    fetch(FEATURE_API)
+      .then((res) => res.json())
+      .then((data) => {
+        setMovie(data.cast);
+        //console.log(data.cast);
+      })
+  })
+  const listItems = movie.map((item) =>
+    <FilmsSliderCard key={item.id} {...item} picSize='130px'/>
+  );
+  return (
+    <div>
+      <h3>Фильмы</h3>
+      <div className={css`
+        display: flex;
+        align-items: baseline;
+        overflow-x: scroll;
+        width: 900px;
+        &>.one-card {
+          margin-right:20px;
+        }
+        & .film-title {
+          font-size: 12px;
+          margin: 0;
+          font-weight:400;
+        }
+        & .vote-average {
+          display: none;
+        }
+        ::-webkit-scrollbar {
+            
+            background-color: transperent;
+            border-radius:2px;
+        }
+        ::-webkit-scrollbar-thumb {
+            background-color: #01b4e4;
+            width: 40%;
+        }
+        ::-webkit-scrollbar-thumb:hover {
+            background-color: #0d253f;
+        }
+      `}>{listItems}</div>
+  </div>
+  );
+}
--- src/components/personalPage/personalPagePeople.js
+++ src/components/personalPage/personalPagePeople.js
@@ -14,6 +14,7 @@ import styled from '@emotion/styled';
 import SmallDropdown from '../smallDropdown';
 import queryString from 'query-string';
 import { useMemo } from "react";
+import MovieCredits from "./movieCredits"
 const IMG_API = "https://image.tmdb.org/t/p/w1280";
 const H2 = styled.h2`
   font-size: 36px;
@@ -83,6 +84,7 @@ export default function PersonalPagePeople(){
           <p className={css`white-space: break-spaces;`}>
           {person.biography}
           </p>  
+          <MovieCredits person_id={person_id}/>
       </div>   
     </div> 
     : ''}   
