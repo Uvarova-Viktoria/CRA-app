28ca456015d734981bec14be400cee1ee923ed99
Uvarova-Viktoria <59088574+Uvarova-Viktoria@users.noreply.github.com>
2021-03-16 18:14:46 +0300

    feat: add search page

---
+++ src/components/search/searchElement.js
@@ -0,0 +1,49 @@
+import React from "react";
+import { css } from '@emotion/css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useHistory,
+  useLocation,
+  useParams,
+  Redirect
+} from "react-router-dom";
+
+export default function SearchElement(){
+  let history = useHistory();
+  function handleClick() {
+    history.push("/search")
+  }
+    return(
+      <form className={css`
+      margin-top: 20px;
+      `}>
+        <input type="text" placeholder="Найти фильм, сериал, персону..." className={css`
+          width: 35%;
+          padding: 10px;
+          padding-left: 20px;
+          outline: none;
+          border: none;
+          border-radius: 24px;
+        `}/>
+        <input type="submit" value="Search" onClick={handleClick} className={css`
+          padding: 10px;
+          outline: none;
+          border: none;
+          border-radius: 24px;
+          width: 6%;
+          position: absolute;
+          right: 35%;
+          background-color: #12ebdc;
+          color: #fff;
+          transition: 0.5s linear;
+          &:hover{
+            background-color:#01b4e4;
+            cursor: pointer;
+          }
+        `}/>
+      </form>
+    );
+}
---
+++ src/components/search/searchPage.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { css } from '@emotion/css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useHistory,
+  useLocation,
+  useParams
+} from "react-router-dom";
+
+export default function SearchPage(){
+  return(
+    <h1>search page</h1>
+  );
+}
